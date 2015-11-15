cee461800cb7c3b35d34afc431bd39f4f3ce20c1
Epirex <estebanguzzo@gmail.com>
2015-11-15 01:46:00 -0300

    Device tree cleanup
    Conflicts:
    	system.prop

--- device_jagnm.mk
+++ device_jagnm.mk
@@ -77,10 +77,6 @@ PRODUCT_PACKAGES += \
     copybit.msm8226 \
     memtrack.msm8226
 
-# Density
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=280
-
 # Keyhandler
 PRODUCT_PACKAGES += \
     com.cyanogenmod.keyhandler
