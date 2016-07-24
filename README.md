5b31430a6abddc6abcc1ecbe2a392cdd4917e7d6
XperiAMM <andresmora1676@gmail.com>
2016-07-24 06:43:43 -0600

    XenonHD-Fy
    Signed-off-by: XperiAMM <andresmora1676@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 LG G3 Beat a.k.a "jagnm" device-tree for MaxiCM
 =============================
 
-Bliss MM Device Tree for LG G3 Beat (D722P), base MaxiCM
+xenonhd MM Device Tree for LG G3 Beat (D722P), base MaxiCM
--- xenonhd.mk
+++ xenonhd.mk
@@ -1,10 +1,10 @@
-# Inherit some common Bliss stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+# Inherit some common xenonhd stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/jagnm/device_jagnm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/xenonhd/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -12,7 +12,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := LG G3 Beat
-PRODUCT_NAME := bliss_jagnm
+PRODUCT_NAME := xenonhd_jagnm
 PRODUCT_MODEL := LG-D722P
 PRODUCT_DEVICE := jagnm
 PRODUCT_MANUFACTURER := LGE
