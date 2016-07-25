7e1d86a0c033f07287eeeb4bb6fefc9c2bef2a41
XperiAMM <andresmora1676@gmail.com>
2016-07-24 22:34:08 -0600

    XOSP
    Signed-off-by: XperiAMM <andresmora1676@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 LG G3 Beat a.k.a "jagnm" device-tree for MaxiCM
 =============================
 
-xenonhd MM Device Tree for LG G3 Beat (D722P), base MaxiCM
+XOSP MM Device Tree for LG G3 Beat (D722P), base MaxiCM
--- xosp.mk
+++ xosp.mk
@@ -1,10 +1,11 @@
-# Inherit some common xenonhd stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+# Inherit some common xosp stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/jagnm/device_jagnm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/xenonhd/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/xosp/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -12,7 +13,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := LG G3 Beat
-PRODUCT_NAME := xenonhd_jagnm
+PRODUCT_NAME := xosp_jagnm
 PRODUCT_MODEL := LG-D722P
 PRODUCT_DEVICE := jagnm
 PRODUCT_MANUFACTURER := LGE
